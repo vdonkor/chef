d11ccbe2d6635cf0334beddac2d442cb4d1bce1e
Donkor <donkorvn@nci-02033583-ml.nci.nih.gov>
2017-12-07 10:12:16 -0500

    sample node app

---
+++ sample-node-app/habitat/plan.sh
@@ -0,0 +1,9 @@
+pkg_name=sample-node-app
+pkg_origin=your_origin
+pkg_scaffolding="core/scaffolding-node"
+pkg_version="1.0.1"
+
+declare -A scaffolding_env
+
+# Define path to config file
+scaffolding_env[APP_CONFIG]="{{pkg.svc_config_path}}/config.json"
